818c3d49e737669f5e5dafb58d681ff19e45a21d
Quinten Jungblut <30625109+Qboi123@users.noreply.github.com>
2020-02-28 18:33:28 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyCompiler',
+    version='1.0.0',
+    packages=['compiler.py'],
+    url='https://github.com/Qboi123/PythonCompiler',
+    license='',
+    author='Qboi123',
+    author_email='',
+    description='Python compiler, compiles python to exe'
+)
